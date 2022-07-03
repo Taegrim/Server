32e977326002026ff64d49c2594e7bfa896a79ef
Taegrim <aopo3948@gmail.com>
2022-07-03 21:57:12 +0900

    Dead Lock
    Lock을 여러개 사용할 때 순서를 맞추지 않는 경우 무한히 기다리는 문제 발생 -> Dead Lock
    
    여러 해결 방법
     - 순서를 하나하나 맞추면서 해결
     - mutex에 번호를 매겨서 번호에 따라 Lock 하도록 해결
     - Graph 구조로 구성하여 Lock이 순환 구조 인지를 추적하여 문제 발생을 파악하도록 함

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -1,68 +1,51 @@
 ﻿// ------------------------------------------------------------------------------------------------------------------------
-// Lock (mutex)
-// 공유 데이터 처리 시 해결 방법
+// Dead Lock
+// 
 #include "pch.h"
 #include <iostream>
 #include "CorePch.h"
 #include <thread>
+#include <atomic>
 #include <mutex>
 #include "save.h"
+#include "AccountManager.h"
+#include "UserManager.h"
 
-vector<int32> v;
-mutex m;
-// Mutual Exclusive (상호 배타적)
-// atomic 처럼 딜레이가 있으므로 동작이 느림
-
-// 유의점 1. 재귀적으로 lock 사용시 recursive_mutex 사용 (일반적인 mutex는 여러번 lock 불가능)
-// 유의점 2. lock 이용 시 unlock을 반드시 해야함 (예외 처리를 할 경우 unlock이 걸리는지에 유의)
-//           -> 일일히 unlock을 하면 복잡함, 빠지는 부분이 있을 수 있음 
-//           -> RAII 방식으로 처리
-// 유의점 3. lock을 거는 범위에 따라 실행이 완전히 달라질 수 있음
-
-// RAII (Resource Aquisition Is Initialization)
-
-template<typename T>
-class LockGuard {
-public:
-	LockGuard(T& m) {
-		_mutex = &m;
-		_mutex->lock();
-	}
-
-	~LockGuard() { _mutex->unlock(); }
-
-private:
-	T* _mutex;
-};
-// 객체 생성에 대한 부담이 있으나 lock, unlock을 수동으로 하는것보다 안정적임
-
-void Push()
+void Func1()
 {
-	// for문 전에 lock을 걸면 Push함수가 끝나야 다른쓰레드가 접근할 수 있어짐
-	for (int32 i = 0; i < 1'0000; ++i) {
-		//LockGuard<std::mutex> lockGuard(m);
-		//std::lock_guard<std::mutex> lockGuard(m);		// 표준 lock_guard
+	for (int32 i = 0; i < 1000; ++i)
+		UserManager::Instance()->ProcessSave();
+}
 
-		std::unique_lock<std::mutex> uniqueLock(m, std::defer_lock);
-		// std::defer_lock 으로 바로 잠그지 않고 lock() 호출 시 잠그도록 할 수 있음
-
-		uniqueLock.lock();
-
-		if (5000 == i)
-			break;
-
-		v.push_back(i);
-	}
+void Func2()
+{
+	for (int32 i = 0; i < 1000; ++i)
+		AccountManager::Instance()->ProcessLogin();
 }
 
 int main()
 {
-	std::thread t1(Push);
-	std::thread t2(Push);
+	std::thread t1(Func1);
+	std::thread t2(Func2);
+
 	t1.join();
 	t2.join();
 
-	cout << v.size() << endl;
+	cout << "Done" << endl;
+	// mutex가 여러개일 때 번호를 지정하여 번호에 따라 순서를 맞추면 어느정도는 해결된다
+
+
+	mutex m1;
+	mutex m2;
+
+	std::lock(m1, m2);
+	// std::lock을 이용하면 내부적으로 m1, m2에 순서를 부여하여 lock을 함
+
+	lock_guard<mutex> g1(m1, std::adopt_lock);
+	lock_guard<mutex> g1(m2, std::adopt_lock);
+	// adopt_lock : 이미 lock된 상태니까, 나중에 소멸될 때 풀어주도록 함
+
+	// lock을 Graph 형식으로 구성하여 사이클이 발생하는지를 추적하여 해결할 수 있음
 
 	//save("GameServer.cpp");
 }
