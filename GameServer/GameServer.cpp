17860286a07422d65b26f3a665a95b3820ee796d
Taegrim <aopo3948@gmail.com>
2022-07-04 23:57:25 +0900

    SpinLock
    Lock 원리 이해 - SpinLock

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -1,5 +1,5 @@
 ﻿// ------------------------------------------------------------------------------------------------------------------------
-// Dead Lock
+// Spin Lock - Lock의 구현 이론
 // 
 #include "pch.h"
 #include <iostream>
@@ -8,44 +8,55 @@
 #include <atomic>
 #include <mutex>
 #include "save.h"
-#include "AccountManager.h"
-#include "UserManager.h"
 
-void Func1()
+class SpinLock 
 {
-	for (int32 i = 0; i < 1000; ++i)
-		UserManager::Instance()->ProcessSave();
+public:
+	void lock() {
+		// CAS (Compare-And-Swap) 
+		bool expected = false;
+		bool desired = true;
+
+		while (!locked.compare_exchange_strong(expected, desired))
+			expected = false;
+	}
+
+	void unlock() {
+		locked.store(false);
+	}
+
+private:
+	atomic<bool> locked = false;
+};
+
+int32 sum = 0;
+SpinLock spinLock;
+
+void Add()
+{
+	for (int32 i = 0; i < 10'0000; ++i) {
+		lock_guard<SpinLock>guard(spinLock);
+		sum++;
+	}
 }
 
-void Func2()
+void Sub()
 {
-	for (int32 i = 0; i < 1000; ++i)
-		AccountManager::Instance()->ProcessLogin();
+	for (int32 i = 0; i < 10'0000; ++i) {
+		lock_guard<SpinLock>guard(spinLock);
+		sum--;
+	}
 }
 
 int main()
 {
-	std::thread t1(Func1);
-	std::thread t2(Func2);
+	thread t1(Add);
+	thread t2(Sub);
 
 	t1.join();
 	t2.join();
 
-	cout << "Done" << endl;
-	// mutex가 여러개일 때 번호를 지정하여 번호에 따라 순서를 맞추면 어느정도는 해결된다
-
-
-	mutex m1;
-	mutex m2;
-
-	std::lock(m1, m2);
-	// std::lock을 이용하면 내부적으로 m1, m2에 순서를 부여하여 lock을 함
-
-	lock_guard<mutex> g1(m1, std::adopt_lock);
-	lock_guard<mutex> g1(m2, std::adopt_lock);
-	// adopt_lock : 이미 lock된 상태니까, 나중에 소멸될 때 풀어주도록 함
-
-	// lock을 Graph 형식으로 구성하여 사이클이 발생하는지를 추적하여 해결할 수 있음
+	cout << sum << endl;
 
 	//save("GameServer.cpp");
 }
