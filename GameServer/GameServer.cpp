6b310f3ee9c7e0063ff8e9547905842d7da00544
Taegrim <aopo3948@gmail.com>
2022-07-07 22:25:54 +0900

    Condition Variable, Future
    condition_variable
    쓰레드간 통신방법, Event를 대체할 수 있음
    
    Future
    단발성 이벤트, 일회용 이벤트에서 유용함
    쓰레드를 직접 생성, 관리하지 않고 future를 통해 처리할 수 있음

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -1,67 +1,84 @@
 ﻿// ------------------------------------------------------------------------------------------------------------------------
-// Event - Lock의 구현 이론
-// 
+// Future
+// mutex, condition_variable 까지 가지 않고 단순한 작업을 처리할 수 있음
+// 일회성 이벤트에 유용함
 #include "pch.h"
 #include <iostream>
 #include "CorePch.h"
 #include <thread>
 #include <atomic>
 #include <mutex>
+#include <future>
 #include "save.h"
-#include <windows.h>
 
-mutex m;
-queue<int32> q;
-HANDLE handle;
-
-void Producer()
+int64 Caculate()
 {
-	while (true) {
-		{
-			unique_lock<mutex> lock(m);
-			q.push(100);
-		}
+	int64 sum = 0;
 
-		::SetEvent(handle);
-		// 이벤트의 상태를 Signal로 바꿈
-		this_thread::sleep_for(100ms);
-	}
+	for (int32 i = 0; i < 100'000; ++i)
+		sum += i;
+
+	return sum;
 }
 
-void Consumer()
+void PromiseWorker(std::promise<string>&& promise)
 {
-	while (true) {
-		::WaitForSingleObject(handle, INFINITE);
-		// handle 에 해당하는 이벤트가 발생할 때까지 INFINITE 만큼 대기함
-		// Event을 Auto로 해두었기 때문에 Non-Signal 로 자동으로 변경함
-		// ::ResetEvent(handle);
+	promise.set_value("Secret Message");
+}
 
-		unique_lock<mutex> lock(m);
-		if (!q.empty()) {
-			int32 data = q.front();
-			q.pop();
-			cout << data << endl;
-		}
-	}
+void TaskWorker(std::packaged_task<int64(void)>&& task)
+{
+	task();
 }
 
 int main()
 {
+	// std::future
+	{
+		std::future<int64> future = std::async(std::launch::async, Caculate);
+		// Caculate를 위한 전용 쓰레드를 만듦
 
-	// 커널 오브젝트
-	// Usage Count
-	// Signal / Non-Signal (켜짐/꺼짐) - bool
-	// Auto / Manual - bool
-	// 보안속성, 수동(true)/자동(flase) 리셋, 초기상태, 이름
-	handle = ::CreateEvent(NULL, false, false, NULL);
+		int64 sum = future.get();
+		cout << sum << endl;
+	}
 
-	thread t1(Producer);
-	thread t2(Consumer);
+	// std::promise  (future를 세팅할 수 있는 객체)
+	{
+		// 미래(std::future)에 결과물을 반환해줄거라 약속(std::promise) 하는 것
+		std::promise<string> promise;
+		std::future<string> future = promise.get_future();
+		// future 추출 
 
-	t1.join();
-	t2.join();
+		thread t(PromiseWorker, std::move(promise));
+		// future는 main쓰레드, promise는 Worker쓰레드가 소유함
 
-	::CloseHandle(handle);
+		string message = future.get();
+		cout << message << endl;
+		// 데이터 추출 시 future 는 empty 상태가 됨 -> 한번만 호출할 수 있음
+
+		t.join();
+	}
+
+	// std::packaged_task
+	{
+		std::packaged_task<int64(void)> task(Caculate);
+		std::future<int64> future = task.get_future();
+
+		std::thread t(TaskWorker, std::move(task));
+		// 이미 존재하고 있는 쓰레드에 일감을 떠넘겨 주는 것(여러개 넘길 수 있음)
+
+		int64 sum = future.get();
+		cout << sum << endl;
+		
+		t.join();
+	}
+
+	// 1) async
+	// 원하는 함수를 비동기적으로 실행
+	// 2) promise
+	// 결과물을 promise를 통해 future로 받아줌
+	// 3) packkaged_task
+	// 원하는 함수의 실행 결과를 packaged_task를 통해 future로 받아줌
 
 	//save("GameServer.cpp");
 }
