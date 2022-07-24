9276844f7175ef642852ed1d2591038d77c63389
Taegrim <aopo3948@gmail.com>
2022-07-24 16:40:31 +0900

    Memory Model, Thread Local Storage
    Memory Model
    atomic한 연산 시 메모리 정책
    일반적으론 순차적으로 처리하기 때문에 가시성, 코드 재배치의 문제가 해결됨
    release, acquire를 쌍으로 배치하여 명시적으로 처리할 수 있음
    
    Thread Local Storage
    쓰레드별 고유의 저장 공간을 Thread Local Storage(TLS) 라고 함
    TLS는 Heap, Data 영역에서 메모리를 읽어와 저장하여 쓰레드 별로 고유의 데이터를 갖도록 함
    - Heap, Data 영역에서 데이터를 가져온 뒤엔 TLS에만 접근하므로 쓰레드 경합이 줄어듦
    -데이터가 변화하더라도 TLS의 값은 변화하지 않으므로 고유의 값으로 처리 가능

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -1,5 +1,5 @@
 ﻿// ------------------------------------------------------------------------------------------------------------------------
-// CPU 파이프라인
+// Thread Local Storage 
 #include "pch.h"
 #include <iostream>
 #include "CorePch.h"
@@ -8,64 +8,35 @@
 #include <mutex>
 #include "save.h"
 
-// 가시성, 코드 재배치
-int32 x = 0;
-int32 y = 0;
-int32 r1 = 0;
-int32 r2 = 0;
 
-volatile bool ready;
+thread_local int32 Threadid = 0;
 
-void Thread_1()
+void ThreadMain(int32 threadid)
 {
-	while (!ready)
-		;
+	Threadid = threadid;
 
-	y = 1;	// Store y
-	r1 = x;	// Load x
-}
-
-void Thread_2()
-{
-	while (!ready)
-		;
-
-	x = 1;	// Store x
-	r2 = y;	// Load y
+	while (true){
+		cout << "Hi I am Thread" << Threadid << endl;
+		this_thread::sleep_for(1s);
+	}
 }
 
 int main()
 {
-	int32 count = 0;
+	vector<thread> threads;
 
-	while (true) {
-		ready = false;
-		count++;
+	for (int i = 0; i < 10; ++i) 
+		threads.push_back(thread(ThreadMain, i + 1));
+	
+	for (thread& t : threads)
+		t.join();
+		
 
-		x = y = r1 = r2 = 0;
-
-		thread t1(Thread_1);
-		thread t2(Thread_2);
-
-		ready = true;
-
-		t1.join();
-		t2.join();
-
-		if (r1 == 0 && r2 == 0)
-			break;
-	}
-
-	cout << count << " 번만에 빠져나옴 " << endl;
-
-	// 가시성 문제
-	// 멀티 쓰레드에선 코어마다 고유의 캐시를 가지고 있음
-	// 따라서 공유 데이터를 사용할 때 그 값이 각각의 캐시에 저장될 수 있고 사용되기 때문에
-	// 한 쓰레드가 공유 데이터의 값을 변경하여도 다른 쓰레드에서 캐시에 저장된 변경전의 데이터를 사용할 수 있다
-
-	// 코드 재배치
-	// 컴파일러/CPU 는 판단하에 작성한 코드의 위치를 수정할 수 있음
-	// -> 단일 쓰레드에서 로직의 결과물이 같다는 보장이 있으면 재배치를 함
+	// Heap, 데이터 영역에 있는 정보를 가져와서 쓰레드마다 각각 가지고 있는 
+	// 쓰레드별 고유의 지역 공간(TLS)에 저장함
+	// 스택은 전역 데이터를 담기에는 불안정하므로 TLS에 가져와서 처리
+	// TLS에 정보를 가져오면 Heap, 데이터 영역에 접근하지 않아도 되므로 쓰레드 경합이 적게 발생함 
+	// 쓰레드 별로 고유의 공간이므로 데이터의 변화가 있어도 TLS의 값은 변하지 않음
 
 	//save("GameServer.cpp");
 }
--- GameServer/save.cpp
+++ GameServer/save.cpp
@@ -7,7 +7,7 @@
 void save(std::string_view filename)
 {
 	std::ifstream in{ filename.data() };
-	std::ofstream out{ ".\\..\\Server 내용 저장.txt", std::ios::app };
+	std::ofstream out{ ".\\..\\..\\Server 내용 저장.txt", std::ios::app };
 
 	char c;
 	in >> std::noskipws;
